a0a1af455e449a045466fd3fc1e51facd5b662fa
iceiceice <597952291@qq.com>
2022-05-10 17:01:22 +0800

    First Commit

---
+++ Q1005/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	spell := [...]string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	var n, sum int
+	var s string
+	_, _ = fmt.Scanf("%d", &n)
+	s = strconv.Itoa(n)
+	for _, v := range s {
+		a, _ := strconv.Atoi(string(v))
+		sum += a
+	}
+	s = strconv.Itoa(sum)
+
+	index, _ := strconv.Atoi(string(s[0]))
+	fmt.Printf("%s", spell[index])
+
+	if sum < 10 {
+		return
+	} else {
+		for _, v := range s[1:] {
+			index, _ = strconv.Atoi(string(v))
+			fmt.Printf(" %s", spell[index])
+		}
+	}
+}
